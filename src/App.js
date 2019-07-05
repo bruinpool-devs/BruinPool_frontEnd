a1a463a27038633f545d1f0ab011c3d81e45b850
jhan25 <justinhan1997@gmail.com>
2019-07-04 21:45:13 -0700

    added routing extensions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import LandingPage from "./components/pages/LandingPage/LandingPage";
+
+import MainState from "./context/MainState";
+
 import "./App.css";
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Welcome to BruinPool</h1>
-    </div>
+    <MainState>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+        </Switch>
+      </Router>
+    </MainState>
   );
 };
 
---
+++ src/components/pages/LandingPage/LandingPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <h1>Welcome to BruinPool</h1>
+      <p>Site is currently under construction...</p>
+    </div>
+  );
+};
+
+export default LandingPage;
