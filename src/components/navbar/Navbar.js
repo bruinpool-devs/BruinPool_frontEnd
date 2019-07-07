e66fa8563abcba4bc5baf544fce5f7af18a7adae
jhan25 <justinhan1997@gmail.com>
2019-07-06 22:33:40 -0700

    completed navbar implementation

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import Sidebar from "react-sidebar";
+import NavBarContent from "./NavbarContent";
+
+const mobileView = window.matchMedia(`(min-width: 800px)`);
+
+const Navbar = ({ children }) => {
+  useEffect(() => {
+    mobileView.addListener(mediaQueryChanged);
+
+    return function cleanup() {
+      mobileView.removeListener(mediaQueryChanged);
+    };
+  });
+
+  const [sidebarDocked, toggleSidebarDocked] = useState(mobileView.matches);
+  const [sidebarOpen, toggleSidebarOpen] = useState(false);
+
+  const onSetSidebarOpen = open => toggleSidebarOpen(open);
+
+  const mediaQueryChanged = () => {
+    toggleSidebarDocked(mobileView.matches);
+    toggleSidebarOpen(false);
+  };
+
+  const styles = {
+    sidebar: {
+      background: "#339FFF",
+      width: "240px",
+      zIndex: "4"
+    }
+  };
+
+  return (
+    <Sidebar
+      sidebar={<NavBarContent />}
+      open={sidebarOpen}
+      docked={sidebarDocked}
+      onSetOpen={onSetSidebarOpen}
+      styles={styles}
+    >
+      {children}
+    </Sidebar>
+  );
+};
+
+export default Navbar;
---
+++ src/components/pages/RiderPage/RiderPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Navbar from "../../navbar/Navbar";
+
+import "./RiderPage.css";
+
+const RiderPage = () => {
+  return (
+    <div>
+      <Navbar>
+        <p>RIDES</p>
+      </Navbar>
+    </div>
+  );
+};
+
+export default RiderPage;
