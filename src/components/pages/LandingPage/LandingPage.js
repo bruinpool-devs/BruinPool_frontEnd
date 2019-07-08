3598c6bf45e45df05e3901672ea78fc6b3cc245b
jhan25 <justinhan1997@gmail.com>
2019-07-07 19:37:24 -0700

    fixed some mobile responsive issues

--- src/components/pages/LandingPage/LandingPage.js
+++ src/components/pages/LandingPage/LandingPage.js
@@ -1,10 +1,19 @@
 import React from "react";
 
+import "./LandingPage.css";
+
 const LandingPage = () => {
   return (
-    <div>
-      <h1>Welcome to BruinPool</h1>
-      <p>Site is currently under construction...</p>
+    <div className="landing-container">
+      <div className="landing-image">
+        <img
+          src={process.env.PUBLIC_URL + "/images/login_photo.jpg"}
+          alt="landing"
+        />
+      </div>
+      <div className="login-section">
+        <h1>Login Form</h1>
+      </div>
     </div>
   );
 };
