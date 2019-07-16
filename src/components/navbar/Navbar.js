68f1e29c5f3bd2f5b1f842d2db96149189699aca
jhan25 <justinhan1997@gmail.com>
2019-07-15 23:32:49 -0700

    test sidebar mobile

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import Sidebar from "react-sidebar";
 import NavBarContent from "./NavbarContent";
+import { Button } from "reactstrap";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const mobileView = window.matchMedia(`(min-width: 1000px)`);
 
@@ -15,12 +18,14 @@ const Navbar = ({ children }) => {
 
   const [sidebarDocked, toggleSidebarDocked] = useState(mobileView.matches);
   const [sidebarOpen, toggleSidebarOpen] = useState(false);
+  const [renderButton, doRenderButton] = useState(!mobileView.matches);
 
   const onSetSidebarOpen = open => toggleSidebarOpen(open);
 
   const mediaQueryChanged = () => {
     toggleSidebarDocked(mobileView.matches);
     toggleSidebarOpen(false);
+    doRenderButton(!mobileView.matches);
   };
 
   const styles = {
@@ -39,6 +44,21 @@ const Navbar = ({ children }) => {
       onSetOpen={onSetSidebarOpen}
       styles={styles}
     >
+      <div className="bars-container">
+        {renderButton && (
+          <Button
+            style={{
+              width: "48px",
+              height: "48px",
+              margin: "3px 0px 0px 20px"
+            }}
+            color="primary"
+            onClick={() => onSetSidebarOpen(true)}
+          >
+            <FontAwesomeIcon icon={faBars} />
+          </Button>
+        )}
+      </div>
       {children}
     </Sidebar>
   );
