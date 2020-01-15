978fd75a4930a874e04f40ef2c73d3d4f8ddeb9b
Erick Suarez <mr.ericksuarez@gmail.com>
2020-01-14 19:39:38 -0800

    Add pre built modal, remove homemade modal

--- src/components/modals/RequestModal/RequestModal.js
+++ src/components/modals/RequestModal/RequestModal.js
@@ -1,141 +1,131 @@
-import React from "react";
+import React, { useState } from "react";
 import Popup from "../PopupModal/PopupModal";
 import { faLongArrowAltRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 
 import "./RequestModal.css";
 
-export default class RequestModal extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { open: false };
-    this.openModal = this.openModal.bind(this);
-    this.closeModal = this.closeModal.bind(this);
-  }
-  openModal() {
-    this.setState({ open: true });
-  }
-  closeModal() {
-    console.log("HELLO");
-    this.setState({ open: false }, function() {
-      console.log("STATE SET TO: ", this.state.open);
-    });
+const RequestModal = props => {
+  const { request } = props;
+
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
+
+  let requestStatusText;
+  let proceedToCheckout;
+  let popUpBtn;
+  const tripSubTotal = props.request.meta.seats * props.request.meta.price;
+
+  switch (props.request.meta.status) {
+    case "pending":
+      requestStatusText = (
+        <span className="orange-highlight">{props.request.meta.status}</span>
+      );
+      popUpBtn = (
+        <Button color="danger" onClick={toggle}>
+          Cancel
+        </Button>
+      );
+      break;
+    case "declined":
+      requestStatusText = (
+        <span className="red-highlight">{props.request.meta.status}</span>
+      );
+      popUpBtn = (
+        <Button color="secondary" onClick={toggle}>
+          Remove
+        </Button>
+      );
+      break;
+    case "cancelled":
+      requestStatusText = (
+        <span className="red-highlight">{props.request.meta.status}</span>
+      );
+      popUpBtn = (
+        <Button color="secondary" onClick={toggle}>
+          Remove
+        </Button>
+      );
+      break;
+    case `approved`:
+      requestStatusText = (
+        <span className="green-highlight">{props.request.meta.status}</span>
+      );
+      popUpBtn = (
+        <Button color="danger" onClick={toggle}>
+          Cancel
+        </Button>
+      );
+      proceedToCheckout = <Button color="primary">Proceed To Checkout</Button>;
+      break;
+    default:
+      requestStatusText = <span className="red-highlight">Invalid Status</span>;
+      break;
   }
 
-  render() {
-    let requestStatusText;
-    let proceedToCheckout;
-    let tripSubTotal =
-      this.props.request.meta.seats * this.props.request.meta.price;
+  return (
+    <div className="request-card" onClick={toggle}>
+      <div className="row request-card-header" style={{ padding: "10px" }}>
+        <div className="col-sm-6" style={{ fontSize: "13px" }}>
+          2 hrs ago
+        </div>
+        <div className="col-sm-6 approved-request-status">
+          {requestStatusText}
+        </div>
+      </div>
 
-    switch (this.props.request.meta.status) {
-      case "pending":
-        requestStatusText = (
-          <span className="orange-highlight">
-            {this.props.request.meta.status}
+      <div className="row card-body">
+        <div className="card-image col-sm-4">
+          <img
+            src={process.env.PUBLIC_URL + "/images/bp_logo.svg"}
+            alt="bear"
+          />
+          <br />
+          <span className="caption card-name">
+            {props.request.ride.ownerFullName}
           </span>
-        );
-        break;
-      case "declined":
-        requestStatusText = (
-          <span className="red-highlight">
-            {this.props.request.meta.status}
-          </span>
-        );
-        break;
-      case "cancelled":
-        requestStatusText = (
-          <span className="red-highlight">
-            {this.props.request.meta.status}
-          </span>
-        );
-        break;
-      case `approved`:
-        requestStatusText = (
-          <span className="green-highlight">
-            {this.props.request.meta.status}
-          </span>
-        );
-        proceedToCheckout = (
-          <span className="default-btn">Proceed To Checkout</span>
-        );
-        break;
-      default:
-        requestStatusText = (
-          <span className="red-highlight">Invalid Status</span>
-        );
-        break;
-    }
-
-    return (
-      <div className="request-card" onClick={this.openModal}>
-        <div className="row request-card-header" style={{ padding: "10px" }}>
-          <div className="col-sm-6" style={{ fontSize: "13px" }}>
-            2 hrs ago
-          </div>
-          <div className="col-sm-6 approved-request-status">
-            {requestStatusText}
-          </div>
         </div>
 
-        <div className="row card-body">
-          <div className="card-image col-sm-4">
-            <img
-              src={process.env.PUBLIC_URL + "/images/bp_logo.svg"}
-              alt="bear"
+        <div className="card-info">
+          <div className="card-itinerary">
+            <div className="itinerary-from">{props.request.ride.from.name}</div>
+            <FontAwesomeIcon
+              icon={faLongArrowAltRight}
+              style={{ width: "50px", height: "30px" }}
             />
-            <br />
-            <span className="caption card-name">
-              {this.props.request.ride.ownerFullName}
-            </span>
+            <div className="itinerary-to">{props.request.ride.to.name}</div>
           </div>
-
-          <div className="card-info">
-            <div className="card-itinerary">
-              <div className="itinerary-from">
-                {this.props.request.ride.from.name}
-              </div>
-              <FontAwesomeIcon
-                icon={faLongArrowAltRight}
-                style={{ width: "50px", height: "30px" }}
-              />
-              <div className="itinerary-to">
-                {this.props.request.ride.to.name}
-              </div>
-            </div>
-            <div>
-              {this.props.request.ride.date} {this.props.request.ride.time}
-            </div>
+          <div>
+            {props.request.ride.date} {props.request.ride.time}
           </div>
         </div>
+      </div>
 
-        <Popup open={this.state.open} onClose={this.closeModal}>
-          <div className="row request-card-header">
-            <div className="col-sm-4 col-md-3 card-itinerary">
-              <div className="itinerary-from">
-                {this.props.request.ride.from.name}
-              </div>
-              <FontAwesomeIcon
-                icon={faLongArrowAltRight}
-                style={{ width: "50px", height: "30px" }}
-              />
-              <div className="itinerary-to">
-                {this.props.request.ride.to.name}
-              </div>
-            </div>
-            <div className="col-md-1"></div>
-            <div className="col-sm-2 col-md-2">{requestStatusText}</div>
-            <div className="col-sm-6 col-md-6"></div>
+      <Modal isOpen={modal} toggle={toggle} size="xl">
+        <ModalHeader toggle={toggle}>
+          <div className="col-sm-4 col-md-3 card-itinerary">
+            <div className="itinerary-from">{props.request.ride.from.name}</div>
+            <FontAwesomeIcon
+              icon={faLongArrowAltRight}
+              style={{ width: "50px", height: "30px" }}
+            />
+            <div className="itinerary-to">{props.request.ride.to.name}</div>
           </div>
-
+          <div className="col-md-1"></div>
+          <div className="col-sm-2 col-md-2">{requestStatusText}</div>
+          <div className="col-sm-6 col-md-6"></div>
+        </ModalHeader>
+        <ModalBody>
           <div className="row request-card-body">
             <div className="request-card-ride-details col-md-4">
-              Date: {this.props.request.ride.date}
-              Time: {this.props.request.ride.time}
-              PickUp: {this.props.request.ride.from.location}
-              DropOff: {this.props.request.ride.to.location}
-              Seats: {this.props.request.meta.seats}
-              Luggage: {this.props.request.meta.luggage}
+              Date: {props.request.ride.date}
+              Time: {props.request.ride.time}
+              PickUp: {props.request.ride.from.location}
+              DropOff: {props.request.ride.to.location}
+              Seats: {props.request.meta.seats}
+              Luggage: {props.request.meta.luggage}
               Trip SubTotal: {tripSubTotal}
             </div>
 
@@ -148,22 +138,27 @@ export default class RequestModal extends React.Component {
                 />
                 <br />
                 <span className="caption card-name">
-                  {this.props.request.ride.ownerFullName}
+                  {props.request.ride.ownerFullName}
                 </span>
               </div>
               Driver's Note:
-              {this.props.request.ride.detail}
+              {props.request.ride.detail}
             </div>
 
             <div className="request-card-request-msg col-md-4">
               <div className="row">
-                Your Message to Driver:{this.props.request.meta.msg}
+                Your Message to Driver:{props.request.meta.msg}
               </div>
-              <div className="row">{proceedToCheckout}</div>
             </div>
           </div>
-        </Popup>
-      </div>
-    );
-  }
-}
+        </ModalBody>
+        <ModalFooter>
+          {proceedToCheckout}
+          {popUpBtn}
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
+};
+
+export default RequestModal;
--- src/components/modules/RequestFeed/RequestFeed.js
+++ src/components/modules/RequestFeed/RequestFeed.js
@@ -6,20 +6,6 @@ import "./RequestFeed.css";
 const _ = require("underscore");
 
 const RequestFeed = ({ requestFeed, userType }) => {
-  const buttonStyle = {
-    backgroundColor: "#3d77ff",
-    borderWidth: "0px",
-    boxShadow: "none",
-    width: "85px",
-    height: "30px",
-    borderRadius: "10px",
-    fontSize: "13px"
-  };
-
-  const redButtonStyle = _.extend({}, buttonStyle, {
-    backgroundColor: "#FF3D3D"
-  });
-
   return (
     <div className="request-container">
       {requestFeed.map(curr_request => {
